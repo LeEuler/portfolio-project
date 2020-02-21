ca4f40594b1b75c6a9f9b3b03128276ec1cd5d05
Serdar <serdaraltuntas@web.de>
2020-02-21 11:17:54 +0100

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'image/')
+
+    def __str__(self):
+        return self.title
+
+    def preview(self):
+        return self.body[:50]
+
+
+    def date(self):
+        return self.pub_date.strftime('%Y %b %-m')
